a5c4a92b20f601ab72b155fed4d64be85f52537f
Fernando Judice <fernandocesar95@gmail.com>
2024-12-26 18:41:40 -0300

    home content

---
+++ src/home.js
@@ -0,0 +1,22 @@
+export const homeRenderer = (function() {
+    const createTitle = () => {
+        const titleCard = document.createElement("div");
+        titleCard.classList.add("home-title")
+        titleCard.textContent = "Minimalist Coffee";
+        return titleCard
+    }
+
+    const createDescription = () => {
+        const descCard = document.createElement("div");
+        descCard.classList.add("home-desc")
+        descCard.textContent = "The true minimalistic experience, so minimal it doesn`t event exist";
+        return descCard
+    }
+
+    const create = (content) => {
+        content.appendChild(createTitle())
+        content.appendChild(createDescription())
+        return content
+    }
+    return {create}
+})();
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 // import "./navbar-style.css";
 import "./styles.css";
 import { navbarRenderer } from "./navbar";
+import { homeRenderer } from "./home";
 
-const navbar = document.querySelector(".navbar")
 
-navbar.appendChild(navbarRenderer.createButton("Home",createCallback("Home")))
-navbar.appendChild(navbarRenderer.createButton("Menu",createCallback("Menu")))
-navbar.appendChild(navbarRenderer.createButton("Contact",createCallback("Contact")))
 
 function createCallback(text) {
     const _text = text;
     const callback = () => {alert(_text)};
     return callback
 }
+
+const createFrame = () => {
+    const mainFrame = document.createElement("div");
+    mainFrame.classList.add("main-frame");
+    const frameContent = document.createElement("div");
+    frameContent.classList.add("frame-content");
+    mainFrame.append(frameContent)
+    return {mainFrame, frameContent}
+}
+
+
+const navbar = document.querySelector(".navbar")
+const content = document.querySelector(".content")
+const {mainFrame, frameContent} = createFrame();
+content.appendChild(mainFrame)
+
+navbar.appendChild(navbarRenderer.createButton("Home",createCallback("Home")));
+navbar.appendChild(navbarRenderer.createButton("Menu",createCallback("Menu")));
+navbar.appendChild(navbarRenderer.createButton("Contact",createCallback("Contact")));
+
+homeRenderer.create(frameContent);
